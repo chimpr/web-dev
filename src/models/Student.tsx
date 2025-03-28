4a92b6b032b5a7a5c78486bdcd807ca752b07eae
albos7 <andres.mateo.albornoz@gmail.com>
2025-03-28 11:25:25 -0400

    Updated the students.tsx
    As requested I created the class Student.tsx that extends user with the information of the student

--- src/models/Student.tsx
+++ src/models/Student.tsx
@@ -1,5 +1,27 @@
-import User from "./User";
+import User, { Role } from "./User";
 
 export default class Student extends User {
-    
+  public school: string;
+  public gradSemester: string;
+  public gradYear: number;
+  public bio: string;
+  public email: string;
+
+  constructor(
+    uid: string,
+    firstName: string,
+    lastName: string,
+    school: string,
+    gradSemester: string,
+    gradYear: number,
+    bio: string,
+    email: string
+  ) {
+    super(uid, "Student", firstName, lastName); 
+    this.school = school;
+    this.gradSemester = gradSemester;
+    this.gradYear = gradYear;
+    this.bio = bio;
+    this.email = email;
+  }
 }
