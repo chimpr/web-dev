4f15b16a8b1a251605f004a23991f1ee5164f157
cataon03 <catalinaocampon03@gmail.com>
2025-04-07 16:07:26 -0400

    added password requirements and fixed margins and sign up boxes

--- src/components/RecruiterSignUp.tsx
+++ src/components/RecruiterSignUp.tsx
@@ -12,6 +12,7 @@ export default function RecruiterSignUp(props: any) {
     }
 
     const [showPassword, setShowPassword] = useState(false);
+    const [passwordFocused, setPasswordFocused] = useState(false);
     const [emailText, setEmailText]       = useState('');
     const [passwordTxt, setPasswordTxt]   = useState('');
     const [firstNameTxt, setFirstNameTxt] = useState('');
@@ -20,11 +21,19 @@ export default function RecruiterSignUp(props: any) {
     const [companyTxt, setCompanyTxt]     = useState('');
     const [emailInvalid, setEmailInvalid] = useState(false);
     const [busy, setBusy]                 = useState(false);
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // todo?
-    const linkedinRegex = /^http:\/\/(www\.)?linkedin\.com\/.*$/;
+    // Password requirement states
+    const passwordRules = {
+        minLength: passwordTxt.length >= 8,
+        hasLower: /[a-z]/.test(passwordTxt),
+        hasUpper: /[A-Z]/.test(passwordTxt),
+        hasNumber: /\d/.test(passwordTxt),
+        hasSpecial: /[!@#$%^&*(),.?":{}|<>]/.test(passwordTxt),
+    };
 
+    const allPasswordValid = Object.values(passwordRules).every(Boolean);
 
     const handleEmailType = (eTxt: string) => {
         setEmailText(eTxt);
@@ -32,55 +41,144 @@ export default function RecruiterSignUp(props: any) {
     }
 
     const signUp = () => {
-        if (formNotFilled || busy)
-            return;
+        if (formNotFilled || busy) return;
+
         setBusy(true);
-        signUpRecruiter(linkedInTxt,companyTxt,firstNameTxt,lastNameTxt,emailText,passwordTxt).then((res) => {
-            if(res["Error"] !== '') {
-                alert("Error: " + res["Error"])
+        signUpRecruiter(linkedInTxt, companyTxt, firstNameTxt, lastNameTxt, emailText, passwordTxt).then((res) => {
+            if (res["Error"] !== '') {
+                alert("Error: " + res["Error"]);
                 setBusy(false);
                 return;
             }
             setBusy(false);
-            alert("Succesfully Created Account!");
-            props.setCurPage(PageType.LOGIN); 
+            alert("Successfully Created Account!");
+            props.setCurPage(PageType.LOGIN);
         });
     }
 
-    const formNotFilled = (emailText === '' || passwordTxt === '' || emailInvalid || firstNameTxt === '' || lastNameTxt === '' || companyTxt === '' || linkedInTxt === '')
+    const formNotFilled = (
+        emailText === '' ||
+        passwordTxt === '' ||
+        emailInvalid ||
+        firstNameTxt === '' ||
+        lastNameTxt === '' ||
+        companyTxt === '' ||
+        linkedInTxt === '' ||
+        !allPasswordValid
+    );
 
-    return <>
-            <div style={{display: 'flex', flexDirection: 'column', height: '100%', width: '100%'}}>
-                <div className="recruiter-sign-up">
-                    <Logo/>
-                    <div className='recruiter-sign-up-content'>
-                        <h1>Recruiter Sign Up</h1>
-                        <p>Sign Up Information</p>
-                        <div style={{width: '100%'}}>
-                            <TextField onChange={(t) => handleEmailType(t.target.value)} style={{width: "75%"}} required label="Email"/>                        
-                            <p style={{color: 'red', display: (emailInvalid ? "" : "none")}}>Invalid email address!</p>
-                        </div>
-                        <div style={{width: '100%', marginTop: '1em', display:'flex', flexDirection: 'column'}}>
-                            <TextField onChange={(t) => setPasswordTxt(t.target.value)} style={{width: "75%", alignSelf: 'top'}} required type={(showPassword) ? "" : "password"} label="Password"/>
-                            <FormControlLabel label="Show Password" control={<Checkbox onChange={() => {setShowPassword(!showPassword)}}/>}/>
-                        </div>
-                        <p>Personal Information</p>
-                        <div className='text-buddies'>
-                            <TextField onChange={(t) => setFirstNameTxt(t.target.value)} required style={{width: '50%'}} label="First Name"/>
-                            <TextField onChange={(t) => setLastNameTxt(t.target.value)} required style={{width: '50%'}} label="Last Name"/>
-                        </div>
-                        <p>Company Information</p>
-                        <div className='text-buddies'>
-                            <TextField onChange={(t) => setLinkedInTxt(t.target.value)} required style={{width: '50%'}} label="LinkedIn"/>
-                            <TextField onChange={(t) => setCompanyTxt(t.target.value)} required style={{width: '50%'}} label="Company"/>
-                        </div>
+    // Helper for styling
+    const passwordStyle = (isValid: boolean) => ({
+        color: isValid ? 'green' : 'red',
+        fontSize: '0.9em',
+        margin: 0,
+    });
+
+    return (
+        <div
+        style={{
+            width: '100%',
+            marginTop: '1em',
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            <div className="recruiter-sign-up">
+                <Logo />
+                <div className="recruiter-sign-up-content"> 
+                    <h1>Recruiter Sign Up</h1>
+                    <p>Sign Up Information</p>
+
+                    <div style={{ width: '100%'}}>
+                        <TextField
+                            onChange={(t) => handleEmailType(t.target.value)}
+                            style={{ width: '75%' }}
+                            required
+                            label="Email"
+                        />
+                        <p style={{ color: 'red', display: emailInvalid ? '' : 'none' }}>Invalid email address!</p>
                     </div>
 
-                    <Button onClick={signUp} disabled={formNotFilled || busy} style={{margin: '1em'}} variant='contained'>Register</Button>
+                    <div style={{ width: '100%', marginTop: '1em', display: 'flex', flexDirection: 'column' }}>
+                        <TextField
+                            onChange={(t) => setPasswordTxt(t.target.value)}
+                            onFocus={() => setPasswordFocused(true)}
+                            onBlur={() => setPasswordFocused(false)}
+                            style={{ width: '75%', alignSelf: 'top' }}
+                            required
+                            type={showPassword ? 'text' : 'password'}
+                            label="Password"
+                        />
+                        <FormControlLabel
+                            label="Show Password"
+                            control={
+                                <Checkbox
+                                    onChange={() => setShowPassword(!showPassword)}
+                                    checked={showPassword}
+                                />
+                            }
+                        />
+
+                        {(passwordFocused || passwordTxt.length > 0) && (
+                            <div style={{ marginTop: '0.5em', paddingLeft: '0.5em' }}>
+                                <p style={passwordStyle(passwordRules.minLength)}>• At least 8 characters</p>
+                                <p style={passwordStyle(passwordRules.hasLower)}>• One lowercase letter</p>
+                                <p style={passwordStyle(passwordRules.hasUpper)}>• One uppercase letter</p>
+                                <p style={passwordStyle(passwordRules.hasNumber)}>• One number</p>
+                                <p style={passwordStyle(passwordRules.hasSpecial)}>• One special character</p>
+                            </div>
+                        )}
+                    </div>
+
+                    <p>Personal Information</p>
+                    <div className="text-buddies">
+                        <TextField
+                            onChange={(t) => setFirstNameTxt(t.target.value)}
+                            required
+                            style={{ width: '50%' }}
+                            label="First Name"
+                        />
+                        <TextField
+                            onChange={(t) => setLastNameTxt(t.target.value)}
+                            required
+                            style={{ width: '50%' }}
+                            label="Last Name"
+                        />
+                    </div>
+
+                    <p>Company Information</p>
+                    <div className="text-buddies">
+                        <TextField
+                            onChange={(t) => setLinkedInTxt(t.target.value)}
+                            required
+                            style={{ width: '50%' }}
+                            label="LinkedIn"
+                        />
+                        <TextField
+                            onChange={(t) => setCompanyTxt(t.target.value)}
+                            required
+                            style={{ width: '50%' }}
+                            label="Company"
+                        />
+                    </div>
                 </div>
-                <Button style={{alignSelf: 'flex-end left'}} onClick={handleGoBackBtnClick} className='go-back-btn'>Go Back</Button>
 
+                <Button
+                    onClick={signUp}
+                    disabled={formNotFilled || busy}
+                    style={{ margin: '1em auto' }} /* Centers the button */
+                    variant="contained"
+                >
+                    Register
+                </Button>
+               
+                <Button
+                    style={{ alignSelf: 'flex-start', margin: '1em' }}
+                    onClick={handleGoBackBtnClick}
+                    className="go-back-btn"
+                >
+                    Go Back
+                </Button>
             </div>
-        </>
-
+        </div>
+    );
 }
